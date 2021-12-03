fbedff8765c79c3f4c3fe581911c04c8c80ccbfd
Cheol-Soon-Choi <79562672+Cheol-Soon-Choi@users.noreply.github.com>
2021-12-03 15:44:35 +0900

    JPA QuerydslPredicateExecutor test

--- src/main/java/com/example/jpa2/entity/ItemRepository.java
+++ src/main/java/com/example/jpa2/entity/ItemRepository.java
@@ -2,11 +2,12 @@ package com.example.jpa2.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface ItemRepository extends JpaRepository<Item, Long> {
+public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item> {
 
     @Query("select i from Item i where i.name like %:name% order by i.price desc ")
     List<Item> findByName(@Param("name") String name);
--- src/test/java/com/example/jpa2/entity/ItemRepositoryTest.java
+++ src/test/java/com/example/jpa2/entity/ItemRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.example.jpa2.entity;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.DisplayName;
@@ -7,6 +8,9 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
@@ -63,5 +67,51 @@ class ItemRepositoryTest {
         for (Item item : itemList) {
             System.out.println(item.toString());
         }
+    }
+
+    public void createItem2() {
+        for (int i = 0; i < 5; i++) {
+            Item item = new Item();
+            item.setName("이름" + i);
+            item.setPrice(10000 + i);
+            item.setDetail("가나다라" + i);
+            itemRepository.save(item);
+        }
+        for (int i = 5; i < 10; i++) {
+            Item item = new Item();
+            item.setName("이름" + i);
+            item.setPrice(10000 + i);
+            item.setDetail("마바사" + i);
+            itemRepository.save(item);
+        }
+    }
+
+    @Test
+    @DisplayName("QuerydslPredicateExecutor 확인")
+    public void QuerydslPredicateExecutorTest() {
+
+        createItem2();
+
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        QItem qitem = QItem.item;
+
+        booleanBuilder.and(qitem.name.like("%" + "이름" + "%"));
+        booleanBuilder.and(qitem.price.gt(10005));
+        booleanBuilder.and(qitem.detail.like("%" + "마바사" + "%"));
+
+        Pageable pageable = PageRequest.of(0, 2);
+        Page<Item> itemPage = itemRepository.findAll(booleanBuilder, pageable);
+
+        System.out.println("getContent= " + itemPage.getContent());
+
+        List<Item> resultItemList = itemPage.getContent();
+        for(Item item: resultItemList){
+            System.out.println(item);
+        }
+
+        System.out.println("getTotalElements= " + itemPage.getTotalElements());
+
+        System.out.println("getTotalPages= " + itemPage.getTotalPages());
+
     }
 }
